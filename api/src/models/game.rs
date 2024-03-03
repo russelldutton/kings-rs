ff507a9d55632fb1420b0be8453c4315376a3e7e
Russell <rdutton1504@gmail.com>
2024-03-03 15:21:12 +0000

    Updated game props

--- api/src/models/game.rs
+++ api/src/models/game.rs
@@ -2,7 +2,7 @@ use super::{card::Card, player::Player, round::Round};
 
 pub struct Game {
     pub session_code: String,
-    pub host: String,
+    pub host: String, // player id
     pub is_started: bool,
     pub rounds: Vec<Round>,
     pub deck: Vec<Card>,
