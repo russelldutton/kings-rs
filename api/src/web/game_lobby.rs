880151a276d1c83551bd8696769710a00f4cbf12
Russell <rdutton1504@gmail.com>
2024-03-23 19:16:12 +0000

    Added lobby routes

---
+++ api/src/web/game_lobby.rs
@@ -0,0 +1,55 @@
+use crate::{
+    common::{app_error::AppError, app_state::AppState},
+    core::game::{create_game_lobby, create_player_in_game, get_game_by_session_code},
+    models::player_model::PlayerModel,
+    util::{game_session_code::generate_random_code, user_session::get_user_id_from_session},
+};
+use axum::{
+    extract::{Path, State},
+    response::Result,
+    routing::get,
+    Json, Router,
+};
+use std::sync::Arc;
+use tower_sessions::Session;
+use tracing::instrument;
+
+pub fn game_lobby_routes() -> Router<Arc<AppState>> {
+    Router::new()
+        .route("/create", get(create_game_lobby_handler))
+        .route("/join/:session_code", get(join_game_lobby_handler))
+}
+
+#[instrument]
+async fn create_game_lobby_handler(
+    session: Session,
+    State(state): State<Arc<AppState>>,
+) -> Result<Json<PlayerModel>, AppError> {
+    let user_id: i64 = get_user_id_from_session(session).await?;
+    let session_code = generate_random_code();
+    let game = create_game_lobby(&state.pool, user_id, session_code).await?;
+    let player = create_player_in_game(&state.pool, user_id, game.id).await?;
+
+    Ok(Json(PlayerModel {
+        id: player.id,
+        game_id: game.id,
+        session_code: game.session_code,
+    }))
+}
+
+#[instrument]
+pub async fn join_game_lobby_handler(
+    session: Session,
+    State(state): State<Arc<AppState>>,
+    Path(session_code): Path<String>,
+) -> Result<Json<PlayerModel>, AppError> {
+    let user_id: i64 = get_user_id_from_session(session).await?;
+    let game = get_game_by_session_code(&state.pool, session_code).await?;
+    let player = create_player_in_game(&state.pool, user_id, game.id).await?;
+
+    Ok(Json(PlayerModel {
+        id: player.id,
+        game_id: game.id,
+        session_code: game.session_code,
+    }))
+}
--- api/src/web/mod.rs
+++ api/src/web/mod.rs
@@ -1,2 +1,3 @@
 pub mod game;
+pub mod game_lobby;
 pub mod user;
